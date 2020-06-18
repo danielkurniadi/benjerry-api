6f45a99b2f8c7800f1a160c9610a50a4d44224c7
iqDF <daniel.thekurniadi@gmail.com>
2020-06-18 23:00:52 +0800

    fix app error message

--- domain/error.go
+++ domain/error.go
@@ -6,9 +6,9 @@ var (
 	// ErrInternalServerError will throw if any the Internal Server Error happen
 	ErrInternalServerError = errors.New("Internal Server Error")
 	// ErrResourceNotFound will throw if the requested item is not exists
-	ErrResourceNotFound = errors.New("Unknown resource or resource not found")
+	ErrResourceNotFound = errors.New("Resource not found")
 	// ErrConflict will throw if the current action already exists
-	ErrConflict = errors.New("Conflicting state")
+	ErrConflict = errors.New("Conflicting state, item with same productId exists")
 	// ErrBadParamInput will throw if the given request input is not valid
 	ErrBadParamInput = errors.New("Bad or invalid input")
 )
