1a1b164962f53ada3a75e306c84dc4d8fd779f68
iqDF <daniel.thekurniadi@gmail.com>
2020-06-20 00:19:03 +0800

    define user roles and permission

---
+++ common/consts/role/role.go
@@ -0,0 +1,15 @@
+package role
+
+const (
+	// Unauthorized ...
+	Unauthorized = "UNAUTHORIZED"
+
+	// ReadPermission ,,,
+	ReadPermission = "READ"
+
+	// WritePermission ...
+	WritePermission = "WRITE"
+
+	// DeletePermission ...
+	DeletePermission = "DELETE"
+)
